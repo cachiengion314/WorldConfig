e4f1f4dbeb332929506e2561667cc52216165a37
HoangNam <hnam143@gmail.com>
2026-08-27 15:49:12 +0700

    update 08-27-2026

---
+++ Runtime/WorldConfig/Components/WorldSystemGroup.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+
+namespace HoangNam.WorldConfig
+{
+  [UpdateInGroup(typeof(InitializationSystemGroup))]
+  public partial class WritableSystemGroup : ComponentSystemGroup { }
+
+  [UpdateInGroup(typeof(SimulationSystemGroup))]
+  public partial class ReadOnlySystemGroup : ComponentSystemGroup { }
+
+  [UpdateInGroup(typeof(PresentationSystemGroup))]
+  public partial class CleanUpSystemGroup : ComponentSystemGroup { }
+}
